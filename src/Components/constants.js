a064e4886c0a3aaa69cc99a86198a6a6222d72f0
Rakesh Kandugula <rakesh.kandugula@stockmann.com>
2025-12-03 11:41:33 +0200

    Add ACC3SS_OY constants to constants.js

--- src/Components/constants.js
+++ src/Components/constants.js
@@ -30,6 +30,10 @@ export const brands = {
     { value: "(RTW)", label: "ADIDAS_SUOMI_OY(RTW)" },
     { value: "(Sports)", label: "ADIDAS_SUOMI_OY(Sports)" }
   ],
+   ACC3SS_OY: [
+    { value: "All Saints", label: "All Saints" },
+    { value: "Ted Baker", label: "Ted Bakery" }
+  ],
   "DK_COMPANY_FINLAND_OY": [
     { value: "Gestuz", label: "Gestuz" },
     { value: "Part Two", label: "Part Two" },
